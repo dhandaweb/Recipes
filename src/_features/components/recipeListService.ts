bbbaf05a8e5f24446c09f798c298e079cd5b345a
dhandaj <dhanda.hunny@gmail.com>
2019-11-05 17:37:34 +1100

    -removed the unwanted file

--- src/_features/components/recipeListService.ts
+++
@@ -1,8 +0,0 @@
-export interface StateProps {
-    recipeListing: any;
-  }
-  export interface DispatchProps {
-    requestMockData: () => void;
-  }
-
-  export interface ComponentProps extends DispatchProps,StateProps{}
